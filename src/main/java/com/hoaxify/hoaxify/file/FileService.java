1cc382ef2434691c026d9e86ef3966f56d2d5713
Zac <thomazac@mail.gvsu.edu>
2021-08-18 19:04:46 -0400

    add scheduled method to clean storage

--- src/main/java/com/hoaxify/hoaxify/file/FileService.java
+++ src/main/java/com/hoaxify/hoaxify/file/FileService.java
@@ -3,7 +3,8 @@ package com.hoaxify.hoaxify.file;
 import com.hoaxify.hoaxify.configuration.AppConfiguration;
 import org.apache.commons.io.FileUtils;
 import org.apache.tika.Tika;
-import org.jetbrains.annotations.NotNull;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,9 +14,11 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Base64;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 @Service
+@EnableScheduling
 public class FileService {
 
     AppConfiguration appConfiguration;
@@ -72,8 +75,28 @@ public class FileService {
                 .build());
     }
 
-    @NotNull
+    // In milliseconds, cleans storage every hour
+    @Scheduled(fixedRate = 60 * 60 * 1000)
+    public void cleanupStorage() {
+        Date oneHourAgo = new Date(System.currentTimeMillis() - (60 * 60 * 1000));
+
+        List<FileAttachment> oldFilesArray = fileAttachmentRepository.findByDateBeforeAndHoaxIsNull(oneHourAgo);
+
+        oldFilesArray.forEach(fileAttachment -> {
+            deleteAttachmentImage(fileAttachment.getName());
+            fileAttachmentRepository.deleteById(fileAttachment.getId());
+        });
+    }
+
     private String getRandomName() {
         return UUID.randomUUID().toString().replace("-", "");
+    }
+
+    private void deleteAttachmentImage(String image) {
+        try {
+            Files.deleteIfExists(Paths.get(appConfiguration.getFullAttachmentsPath() + "/" + image));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/test/java/com/hoaxify/hoaxify/FileServiceTest.java
+++ src/test/java/com/hoaxify/hoaxify/FileServiceTest.java
@@ -1,21 +1,28 @@
 package com.hoaxify.hoaxify;
 
 import com.hoaxify.hoaxify.configuration.AppConfiguration;
+import com.hoaxify.hoaxify.file.FileAttachment;
+import com.hoaxify.hoaxify.file.FileAttachmentRepository;
 import com.hoaxify.hoaxify.file.FileService;
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Date;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+// Unit Tests
 @RunWith(SpringRunner.class)
 @ActiveProfiles("test")
 public class FileServiceTest {
@@ -24,12 +31,15 @@ public class FileServiceTest {
 
     AppConfiguration appConfiguration;
 
+    @MockBean
+    FileAttachmentRepository fileAttachmentRepository;
+
     @Before
-    public void init(){
+    public void init() {
         appConfiguration = new AppConfiguration();
         appConfiguration.setUploadPath("uploads-test");
 
-        fileService = new FileService(appConfiguration, null);
+        fileService = new FileService(appConfiguration, fileAttachmentRepository);
 
         new File(appConfiguration.getUploadPath()).mkdir();
         new File(appConfiguration.getFullProfileImagesPath()).mkdir();
@@ -51,4 +61,42 @@ public class FileServiceTest {
 
         assertThat(fileType).isEqualToIgnoringCase("image/png");
     }
+
+    @Test
+    public void cleanupStorage_whenOldFilesExist_removeFilesFromStorage() throws IOException {
+        String fileName = "random-file";
+        String filePath = appConfiguration.getFullAttachmentsPath() + "/" + fileName;
+        File source = new ClassPathResource("profile.png").getFile();
+        File target = new File(filePath);
+        FileUtils.copyFile(source, target);
+
+        FileAttachment fileAttachment = new FileAttachment();
+        fileAttachment.setId(5);
+        fileAttachment.setName(fileName);
+
+        Mockito.when(fileAttachmentRepository.findByDateBeforeAndHoaxIsNull(Mockito.any(Date.class))).thenReturn(Arrays.asList(fileAttachment));
+
+        fileService.cleanupStorage();
+        File storedImage = new File(filePath);
+        assertThat(storedImage.exists()).isFalse();
+    }
+
+    @Test
+    public void cleanupStorage_whenOldFilesExist_removeFileAttachmentFromDatabase() throws IOException {
+        String fileName = "random-file";
+        String filePath = appConfiguration.getFullAttachmentsPath() + "/" + fileName;
+        File source = new ClassPathResource("profile.png").getFile();
+        File target = new File(filePath);
+        FileUtils.copyFile(source, target);
+
+        FileAttachment fileAttachment = new FileAttachment();
+        fileAttachment.setId(5);
+        fileAttachment.setName(fileName);
+
+        Mockito.when(fileAttachmentRepository.findByDateBeforeAndHoaxIsNull(Mockito.any(Date.class))).thenReturn(Arrays.asList(fileAttachment));
+
+        fileService.cleanupStorage();
+        Mockito.verify(fileAttachmentRepository).deleteById(5L);
+    }
+
 }
